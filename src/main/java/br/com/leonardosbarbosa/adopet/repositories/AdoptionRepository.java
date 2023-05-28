b8b77527cca692b3ea0f9f7577f343ad38c9f441
Leonardo Barbosa <leonardo.sbarbosa2801@gmail.com>
2023-05-27 21:27:00 -0300

    Improving adoption resources

--- src/main/java/br/com/leonardosbarbosa/adopet/repositories/AdoptionRepository.java
+++ src/main/java/br/com/leonardosbarbosa/adopet/repositories/AdoptionRepository.java
@@ -2,6 +2,10 @@ package br.com.leonardosbarbosa.adopet.repositories;
 
 import br.com.leonardosbarbosa.adopet.entities.Adoption;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface AdoptionRepository extends JpaRepository<Adoption, Long> {
+
+    @Query(value = "SELECT a.pet.id FROM Adoption a WHERE a.id = :id")
+    Long findPetIdByAdoptionId(Long id);
 }
--- src/main/java/br/com/leonardosbarbosa/adopet/services/AdoptionService.java
+++ src/main/java/br/com/leonardosbarbosa/adopet/services/AdoptionService.java
@@ -4,7 +4,9 @@ import br.com.leonardosbarbosa.adopet.config.errors.exceptions.DatabaseException
 import br.com.leonardosbarbosa.adopet.config.errors.exceptions.ResourceNotFoundException;
 import br.com.leonardosbarbosa.adopet.dto.AdoptionDTO;
 import br.com.leonardosbarbosa.adopet.entities.Adoption;
+import br.com.leonardosbarbosa.adopet.entities.Pet;
 import br.com.leonardosbarbosa.adopet.repositories.AdoptionRepository;
+import br.com.leonardosbarbosa.adopet.repositories.PetRepository;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
@@ -14,14 +16,17 @@ import org.springframework.stereotype.Service;
 import java.time.Instant;
 
 import static br.com.leonardosbarbosa.adopet.config.errors.messages.AdoptionErrorMessages.*;
+import static br.com.leonardosbarbosa.adopet.config.errors.messages.PetErrorMessages.NONEXISTENT_PET_MESSAGE;
 
 @Service
 public class AdoptionService {
 
     private final AdoptionRepository adoptionRepository;
+    private final PetRepository petRepository;
 
-    public AdoptionService(AdoptionRepository adoptionRepository) {
+    public AdoptionService(AdoptionRepository adoptionRepository, PetRepository petRepository) {
         this.adoptionRepository = adoptionRepository;
+        this.petRepository = petRepository;
     }
 
     public Page<AdoptionDTO> findAll(Pageable pageRequest) {
@@ -45,6 +50,7 @@ public class AdoptionService {
             Adoption entity = new Adoption(adoption);
             entity.setDate(Instant.now());
             entity = adoptionRepository.save(entity);
+            setPetAdoptionStatus(entity.getPet().getId(), true);
             return new AdoptionDTO(entity);
         } catch (DataIntegrityViolationException e) {
             throw new DatabaseException(DEFAULT_ADOPTION_INTEGRITY_VIOLATION_MESSAGE);
@@ -53,11 +59,20 @@ public class AdoptionService {
 
     public void deleteById(Long id) {
         try {
+            Long petId = adoptionRepository.findPetIdByAdoptionId(id);
+            setPetAdoptionStatus(petId, false);
             adoptionRepository.deleteById(id);
         } catch (EmptyResultDataAccessException e) {
             throw new ResourceNotFoundException(NONEXISTENT_ADOPTION_MESSAGE);
         } catch (DataIntegrityViolationException e) {
             throw new DatabaseException(DELETE_ADOPTION_INTEGRITY_VIOLATION_MESSAGE);
         }
+    }
+
+    public void setPetAdoptionStatus(Long petId, boolean isAdopted) {
+        Pet adoptedPet = petRepository.findById(petId)
+                .orElseThrow(() -> new ResourceNotFoundException(NONEXISTENT_PET_MESSAGE));
+        adoptedPet.setAdopted(isAdopted);
+        petRepository.save(adoptedPet);
     }
 }
