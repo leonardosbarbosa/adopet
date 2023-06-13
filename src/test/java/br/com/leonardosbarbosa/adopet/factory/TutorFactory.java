08a962c0997aae46aa4ec663a836ed5168a34415
Leonardo Barbosa <leonardo.sbarbosa2801@gmail.com>
2023-06-13 20:45:05 -0300

    TutorService: find by id tests

--- src/test/java/br/com/leonardosbarbosa/adopet/factory/TutorFactory.java
+++ src/test/java/br/com/leonardosbarbosa/adopet/factory/TutorFactory.java
@@ -5,11 +5,14 @@ import br.com.leonardosbarbosa.adopet.entities.Tutor;
 public class TutorFactory {
 
     public static Tutor createTutor() {
-        return Tutor.builder()
+        Tutor tutor = Tutor.builder()
                 .fullName("Leonardo Barbosa")
                 .about("Desenvolvedor de Software")
                 .phone("11945154979")
                 .city("São Paulo")
                 .build();
+
+        tutor.setId(1L);
+        return tutor;
     }
 }
--- src/test/java/br/com/leonardosbarbosa/adopet/services/TutorServiceTests.java
+++ src/test/java/br/com/leonardosbarbosa/adopet/services/TutorServiceTests.java
@@ -1,10 +1,10 @@
 package br.com.leonardosbarbosa.adopet.services;
 
+import br.com.leonardosbarbosa.adopet.config.errors.exceptions.ResourceNotFoundException;
 import br.com.leonardosbarbosa.adopet.dto.TutorDTO;
 import br.com.leonardosbarbosa.adopet.entities.Tutor;
 import br.com.leonardosbarbosa.adopet.factory.TutorFactory;
 import br.com.leonardosbarbosa.adopet.repositories.TutorRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatchers;
@@ -18,10 +18,10 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.times;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class TutorServiceTests {
@@ -30,20 +30,37 @@ public class TutorServiceTests {
     private TutorService tutorService;
     @Mock
     private TutorRepository tutorRepository;
+    @Mock
+    private AuthService authService;
 
-    @BeforeEach
-    public void setUp() {
-        Tutor tutor = TutorFactory.createTutor();
-        Page<Tutor> page = new PageImpl<>(List.of(tutor));
-        Mockito.when(tutorRepository.findAll(ArgumentMatchers.any(PageRequest.class))).thenReturn(page);
-    }
+    private final Tutor tutor = TutorFactory.createTutor();
+    private final Long existentId = 1L;
+    private final Long nonexistentId = 2L;
+    private final Page<Tutor> page = new PageImpl<>(List.of(tutor));
+
 
     @Test
     public void findAllShouldReturnPage() {
+        when(tutorRepository.findAll(ArgumentMatchers.any(PageRequest.class))).thenReturn(page);
         Pageable pageable = PageRequest.of(0, 20);
         Page<TutorDTO> result = tutorService.findAll(pageable);
         assertNotNull(result);
         assertEquals(1, result.getTotalElements());
         Mockito.verify(tutorRepository, times(1)).findAll(pageable);
+    }
+
+    @Test
+    public void findByIdShouldReturnTutorDTO() {
+        when(tutorRepository.findById(existentId)).thenReturn(Optional.of(tutor));
+        doNothing().when(authService).validateSelfOrAdmin(anyLong());
+        TutorDTO response = tutorService.findById(existentId);
+        assertNotNull(response);
+    }
+
+    @Test
+    public void findByIdShouldThrowResourceNotFoundException() {
+        when(tutorRepository.findById(nonexistentId)).thenReturn(Optional.empty());
+
+        assertThrows(ResourceNotFoundException.class, () -> tutorService.findById(nonexistentId));
     }
 }
